177003564ca17a26ea25f88b6f4c9c3b093ec840
fawzy1289 <ahmada3435@hotmail.com>
2025-10-28 23:52:15 +0300

    Add Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { footerLinks } from "../constants";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return(
+    <footer>
+      <div className="info">
+        <p>More ways to shop: <span>Find an Apple Store</span> or <span>other retailer</span> near you. Or call 1-800-MY-APPLE.</p>
+        <img src='/logo.svg' alt='apple logo' />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2025 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map((link) => (
+            <li key={link.label}>
+              <a href={link.link}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
